92c8c15066eae3888caa98399d45eddf69a10267
Arnold Daniels <arnold@jasny.net>
2015-11-04 17:48:27 -0400

    Fixed issue when inject-template is minified

--- src/inject-template.js
+++ src/inject-template.js
@@ -1,47 +1,50 @@
 angular.module('injectTemplate', []);
 
-angular.module('injectTemplate').factory('injectTemplate', function($compile, $sce, $templateRequest) {
-  'use strict';
-  return function (element, css, source, scope){
-    var elements;
+angular.module('injectTemplate').factory('injectTemplate', [
+  '$compile', '$sce', '$templateRequest',
+  function($compile, $sce, $templateRequest) {
+    'use strict';
+    return function (element, css, source, scope){
+      var elements;
 
-    if ('querySelectorAll' in element) {
-      elements = element.querySelectorAll(css);
-    } else {
-      elements = element.find(css);
-    }
+      if ('querySelectorAll' in element) {
+        elements = element.querySelectorAll(css);
+      } else {
+        elements = element.find(css);
+      }
 
-    if (typeof source === 'string') {
-      source = { templateUrl: source };
-    }
+      if (typeof source === 'string') {
+        source = { templateUrl: source };
+      }
 
-    function loadTemplate(template) {
-      angular.forEach(elements, function(element){
-        if (template) {
-          angular.element(element).append(template);
-        }
-        
-        if (source.attr) {
-          angular.forEach(source.attr, function(value, key) {
-            angular.element(element).attr(key, value);
-          });
-        }
-        
-        var childScope = typeof scope === 'function' ? scope() : scope;
-        $compile(element)(childScope);
-      });
-    }    
-    
-    if (source.templateUrl) {
-      var templateUrl = $sce.getTrustedResourceUrl(source.templateUrl);
-      $templateRequest(templateUrl).then(
-        loadTemplate,
-        function() {
-          throw new Error('could not find template');
-        }
-      );
-    } else {
-      loadTemplate(source.template);
-    }
-  };
-});
+      function loadTemplate(template) {
+        angular.forEach(elements, function(element){
+          if (template) {
+            angular.element(element).append(template);
+          }
+          
+          if (source.attr) {
+            angular.forEach(source.attr, function(value, key) {
+              angular.element(element).attr(key, value);
+            });
+          }
+          
+          var childScope = typeof scope === 'function' ? scope() : scope;
+          $compile(element)(childScope);
+        });
+      }    
+      
+      if (source.templateUrl) {
+        var templateUrl = $sce.getTrustedResourceUrl(source.templateUrl);
+        $templateRequest(templateUrl).then(
+          loadTemplate,
+          function() {
+            throw new Error('could not find template');
+          }
+        );
+      } else {
+        loadTemplate(source.template);
+      }
+    };
+  }
+]);
